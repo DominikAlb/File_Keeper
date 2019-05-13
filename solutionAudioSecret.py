8979094c6a859bf08c893fe936647037b8b13cae
Dominik Albiniak <dominik.albiniak@o2.pl>
2019-05-13 18:19:41 +0200

    Delete solutionAudioSecret.py

--- solutionAudioSecret.py
+++
@@ -1,8 +0,0 @@
-
-import wave
-def func():
-    song = wave.open("song_embedded.wav", mode='rb')
-    frame_bytes = bytearray(list(song.readframes(song.getnframes())))
-    #...
-    song.close()
-func()
